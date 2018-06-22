0e09ea62e3aab390e86867d8993b3ad9f1668fb4
Prog84 <bvv198424@rambler.ru>
2018-06-22 12:34:06 +1000

    setup passport

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
 const container = require('./container');
-
-
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('flash');
+const passport = require('passport');
 
 container.resolve(function(users){
+
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/fullappchat', {useMongoClient: true});
 
     const app = SetupExpress();
 
@@ -24,8 +32,22 @@ container.resolve(function(users){
     }
     function ConfigureExpress(app){
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+
+        app.use(validator());
+        app.use(session({
+            secret: 'mybigsecretkey',
+            resave: true,
+            saveInitialized: true,
+            store: new MongoStore({mongooseConnection: mongoose.connection})
+        }));
+
+        app.use(flash());
+
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
 });
